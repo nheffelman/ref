1620e6f49833dfdd7bc02423bcff0a67a1dd00cd
Nathan Heffelman <nheffelman@gmail.com>
2013-05-19 11:57:32 -0500

    with shaders and canvas and draw

--- RefsBitch/gen/com/example/RefsBitch/R.java
+++ RefsBitch/gen/com/example/RefsBitch/R.java
@@ -30,15 +30,16 @@ public final class R {
         public static final int blue2=0x7f020005;
         public static final int gray=0x7f020006;
         public static final int green=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int indigo=0x7f020009;
-        public static final int orange=0x7f02000a;
-        public static final int pink=0x7f02000b;
-        public static final int red=0x7f02000c;
-        public static final int soccer_field=0x7f02000d;
-        public static final int violet=0x7f02000e;
-        public static final int white=0x7f02000f;
-        public static final int yellow=0x7f020010;
+        public static final int green1=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int indigo=0x7f02000a;
+        public static final int orange=0x7f02000b;
+        public static final int pink=0x7f02000c;
+        public static final int red=0x7f02000d;
+        public static final int soccer_field=0x7f02000e;
+        public static final int violet=0x7f02000f;
+        public static final int white=0x7f020010;
+        public static final int yellow=0x7f020011;
     }
     public static final class id {
         public static final int action_settings=0x7f09000a;
